4d6d04220cc4f5b086fbf8ded50a3cc5f408dd95
Mester Zsolt <zsolt.mester67@gmail.com>
2015-02-20 19:55:06 +0100

    Test commit

--- app/src/main/java/windroids/semifinal/MainActivity.java
+++ app/src/main/java/windroids/semifinal/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		// test TODO delete
 		setContentView(R.layout.activity_main);
 	}
 
