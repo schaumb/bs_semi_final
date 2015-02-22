5be6d5e8573c05c04abe4ee57e62df6e711c09dd
alitams <ali.tams@gmail.com>
2015-02-22 15:06:37 +0100

    New commit

--- app/src/main/java/windroids/semifinal/ui/MainActivity.java
+++ app/src/main/java/windroids/semifinal/ui/MainActivity.java
@@ -3,6 +3,7 @@ package windroids.semifinal.ui;
 import org.xmlpull.v1.XmlPullParserException;
 
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -26,194 +27,218 @@ import windroids.semifinal.ui.keyboard.Keyboard;
 import windroids.semifinal.util.Config;
 
 public class MainActivity extends ActionBarActivity implements Keyboard.EventListener {
-	
-	private TextView inputView;
-	private TextView infoView;
-	private TextView counterView;
 
-	private State currentState;
-	
-	private enum State {
-		START,
-		TRAINING,
-		TEST
-	}
-	
-	private String password;
-	private List<Pattern> training = new ArrayList<>(100);
-	private Logic logic;
+    private TextView inputView;
+    private TextView infoView;
+    private TextView counterView;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		Keyboard keyboard = new Keyboard();
-		keyboard.setEventListener(this);
-		getFragmentManager().beginTransaction().add(R.id.keyboard_container, keyboard, null).commit();
-		
-		inputView = (TextView) findViewById(R.id.input_view);
-		infoView = (TextView) findViewById(R.id.description_view);
-		counterView = (TextView) findViewById(R.id.counter_view);
-		
-		changeToStartState();
+    private State currentState;
 
-		// teszt a kliens-szerver kommunikaciora. Kerlek allitsd be a Configban az ip cimedet, es a
-		// futtatasa elott inditsd el a szervert (java -jar ECServer.jar)
-//        testCommunication();
-	}
-	
-	private void changeToStartState() {
-		infoView.setText(R.string.start_instructions);
-		currentState = State.START;
-		password = null;
-		training = new ArrayList<>(100);
-		logic = null;
-	}
+    private enum State {
+        START,
+        TRAINING,
+        TEST
+    }
 
-	private void changeToTrainingState() {
-		infoView.setText(R.string.training_instructions);
-		currentState = State.TRAINING;
-		startCounter();
-	}
+    private String password;
+    private List<Pattern> training = new ArrayList<>(100);
+    private Logic logic;
 
-	private void changeToTestState() {
-		infoView.setText(R.string.test_instructions);
-		currentState = State.TEST;
-	}
-	
-	public void finishTraining() {
-		logic = new Logic(password, training);
-		logic.init();
-		changeToTestState();
-	}
-	
-	private void startCounter() {
-		new AsyncTask<Void, Integer, Void>() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-			@Override
-			protected Void doInBackground(Void... params) {
-				counterView.setVisibility(View.VISIBLE);
-				for (int i = 0; i < 30; ++i) {
-					publishProgress(i);
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-				return null;
-			}
+        Keyboard keyboard = new Keyboard();
+        keyboard.setEventListener(this);
+        getFragmentManager().beginTransaction().add(R.id.keyboard_container, keyboard, null).commit();
 
-			@Override
-			protected void onProgressUpdate(Integer... values) {
-				super.onProgressUpdate(values);
-				counterView.setText(String.valueOf(values[0]));
-			}
+        inputView = (TextView) findViewById(R.id.input_view);
+        infoView = (TextView) findViewById(R.id.description_view);
+        counterView = (TextView) findViewById(R.id.counter_view);
 
-			@Override
-			protected void onPostExecute(Void aVoid) {
-				super.onPostExecute(aVoid);
-				counterView.setVisibility(View.INVISIBLE);
-				finishTraining();
-			}
-		}.execute((Void[]) null);
-	}
+        changeToStartState();
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String testData;
+        switch (item.getItemId()) {
+            case R.id.menu_test1:
+                testData = "TEST1";
+                break;
+            case R.id.menu_test2:
+                testData = "TEST2";
+                break;
+            case R.id.menu_test3:
+                testData = "TEST3";
+                break;
+            case R.id.menu_test4:
+                testData = "TEST4";
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        startCommunication(Config.COMM_HOST, Config.COMM_PORT, testData, true);
+        return super.onOptionsItemSelected(item);
+    }
 
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
+    private void changeToStartState() {
+        infoView.setText(R.string.start_instructions);
+        currentState = State.START;
+        password = null;
+        training = new ArrayList<>(100);
+        logic = null;
+    }
 
-		return super.onOptionsItemSelected(item);
-	}
+    private void changeToTrainingState() {
+        infoView.setText(R.string.training_instructions);
+        currentState = State.TRAINING;
+        startCounter();
+    }
 
-	private void testCommunication() {
-		Communicator communicator = null;
-		try {
-			communicator = new Communicator(Config.COMM_HOST, Config.COMM_PORT, "TEST3");
-			communicator.startCommunication();
-			String password = communicator.doPreCommuncationAndGetPassword();
-			String patternListRaw = communicator.getXmlFromServer();
-			List<Pattern> patternList = XmlParser.parseTestData(patternListRaw);
-			String nextTestDataRaw = communicator.getNextTestDataXmlFile();
-			while (!communicator.checkEndMessage(nextTestDataRaw)) {
-				Pattern pattern = XmlParser.parsePattern(nextTestDataRaw);
-				Log.d(Config.LOG, "Pattern events number: " + pattern.getEvents().size());
-				nextTestDataRaw = communicator.answerTestDataAndGetNext(true);
-			}
-			communicator.endCommuncation();
+    private void changeToTestState() {
+        infoView.setText(R.string.test_instructions);
+        currentState = State.TEST;
+    }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			e.printStackTrace();
-		} catch (XmlPullParserException e) {
-			e.printStackTrace();
-		}
-	}
+    public void finishTraining() {
+        logic = new Logic(password, training);
+        logic.init();
+        changeToTestState();
+    }
 
-	@Override
-	public void onTextInput(String character) {
-		String currentText = inputView.getText().toString();
-		currentText += character;
-		inputView.setText(currentText);
-	}
+    private void startCounter() {
+        new AsyncTask<Void, Integer, Void>() {
 
-	@Override
-	public void onBackspace() {
-		String currentText = inputView.getText().toString();
-		
-		if (currentText.length() == 0) {
-			return;
-		} else if (currentText.length() == 1) {
-			inputView.setText("");
-			return;
-		}
-			
-		currentText = currentText.substring(0, currentText.length() - 1);
-		inputView.setText(currentText);
-	}
+            @Override
+            protected Void doInBackground(Void... params) {
+                counterView.setVisibility(View.VISIBLE);
+                for (int i = 0; i < 30; ++i) {
+                    publishProgress(i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                return null;
+            }
 
-	@Override
-	public void onSubmit(Pattern pattern) {
-		switch (currentState) {
-			case START:
-				password = inputView.getText().toString();
-				changeToTrainingState();
-				break;
-			case TRAINING:
-				training.add(pattern);
-				break;
-			case TEST:
-				if (logic.isMatching(pattern)) {
-					new AlertDialog.Builder(this)
-							.setMessage(R.string.successful_auth)
-							.create()
-							.show();
-				} else {
-					new AlertDialog.Builder(this)
-							.setMessage(R.string.fail_auth)
-							.create()
-							.show();
-				}
-		}
-		inputView.setText("");
-	}
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                super.onProgressUpdate(values);
+                counterView.setText(String.valueOf(values[0]));
+            }
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                counterView.setVisibility(View.INVISIBLE);
+                finishTraining();
+            }
+        }.execute((Void[]) null);
+    }
+
+    private void startCommunication(String hostName, int portName, String dataName, boolean feedback) {
+        Communicator communicator = null;
+        String password;
+        List<Pattern> patternList;
+        Pattern actualPattern;
+        try {
+            communicator = new Communicator(hostName, portName, dataName);
+            if(communicator.startCommunication()) {
+                password = communicator.doPreCommuncationAndGetPassword();
+                String patternListRaw = communicator.getXmlFromServer();
+                patternList = XmlParser.parseTestData(patternListRaw);
+                String nextTestDataRaw = communicator.getNextTestDataXmlFile();
+                while (!communicator.checkEndMessage(nextTestDataRaw)) {
+                    actualPattern = XmlParser.parsePattern(nextTestDataRaw);
+                    Log.d(Config.LOG, "Pattern events number: " + actualPattern.getEvents().size());
+                    nextTestDataRaw = communicator.answerTestDataAndGetNext(true);
+                }
+                if (feedback) {
+                    alertDialog(nextTestDataRaw);
+                }
+                communicator.endCommuncation();
+            } else {
+                alertDialog("Server communication failed!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void alertDialog(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        return;
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+
+    @Override
+    public void onTextInput(String character) {
+        String currentText = inputView.getText().toString();
+        currentText += character;
+        inputView.setText(currentText);
+    }
+
+    @Override
+    public void onBackspace() {
+        String currentText = inputView.getText().toString();
+
+        if (currentText.length() == 0) {
+            return;
+        } else if (currentText.length() == 1) {
+            inputView.setText("");
+            return;
+        }
+
+        currentText = currentText.substring(0, currentText.length() - 1);
+        inputView.setText(currentText);
+    }
+
+    @Override
+    public void onSubmit(Pattern pattern) {
+        switch (currentState) {
+            case START:
+                password = inputView.getText().toString();
+                changeToTrainingState();
+                break;
+            case TRAINING:
+                training.add(pattern);
+                break;
+            case TEST:
+                if (logic.isMatching(pattern)) {
+                    new AlertDialog.Builder(this)
+                            .setMessage(R.string.successful_auth)
+                            .create()
+                            .show();
+                } else {
+                    new AlertDialog.Builder(this)
+                            .setMessage(R.string.fail_auth)
+                            .create()
+                            .show();
+                }
+        }
+        inputView.setText("");
+    }
 }
