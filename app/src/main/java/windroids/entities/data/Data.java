cec8bb33a75228ce53101d1f27c7728a6373d845
unknown <ebelsch@HU00115349.ericsson.se>
2015-03-07 22:14:41 +0100

    bloodpressure code, heartrate fragment from example

--- app/src/main/java/windroids/entities/data/Data.java
+++ app/src/main/java/windroids/entities/data/Data.java
@@ -1,17 +1,20 @@
 package windroids.entities.data;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Data implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    public enum Type {Common, Pulse};
+    public enum Type {Common, Pulse, BloodPressure};
 
     public Data(Type type) {
         this.type = type;
+        this.date = new Date();
     }
 
     private Type type;
+    private Date date;
 
     public Type getType() {
         return type;
